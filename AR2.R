066f041fbdac3a7b4bab39141d9027127d787f58
DineshDhule7 <101201476+DineshDhule7@users.noreply.github.com>
2022-03-10 12:59:10 +0530

    Add files via upload

---
+++ AR2.R
@@ -0,0 +1,12 @@
+st_name <- "andrew"
+st_age <- 16
+st_class <- 10
+st_gender <- "male"
+t_name <- "angelina"
+t_age <- 26
+t_gender <- "female"
+student <- c(st_name, st_age, st_class, st_gender)
+teacher <- c(t_name, t_age, t_gender)
+print(student)
+print(teacher)
+
---
+++ py3.py
@@ -0,0 +1,3 @@
+list1 = [1,2,3,4,5,6,7,8,9]
+even_nos = [num for num in list1 if num % 2==0]
+print('Even numbers :', even_nos)  
